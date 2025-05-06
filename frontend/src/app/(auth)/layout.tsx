bb38bad18fe8a470599d70f9215b793dbd296a72
24LP0262 24LP0262 <24LP0262@Setups-iMac.local>
2025-05-06 16:38:54 +0800

    not yet done. Started again

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,12 +1,12 @@
 import {CoffeeLogoSection} from "./createAccount/component/CoffeeLogoSection";
-
+import {Button} from "@/components/ui/button";
 export default function AuthLayout({children}: {children: React.ReactNode}) {
   return (
     <div className="flex h-screen">
       <div className="flex w-[50%]">
         <CoffeeLogoSection />
       </div>
-      <div className="flex w-[50%]">{children}</div>
+      <div className="flex w-[50%] h-full">{children}</div>
     </div>
   );
 }
